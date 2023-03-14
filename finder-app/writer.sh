a8c900081bb97082bc8ddd8eb0294cc5567e4778
Salik Satti <34483524+satti001@users.noreply.github.com>
2023-03-14 15:37:17 +0500

    Add files via upload

---
+++ finder-app/writer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Check if both arguments were provided
+if [ $# -ne 2 ]; then
+  echo "Provide two arguments"
+  exit 1
+fi
+
+# Assign arguments to variables
+writefile=$1
+writestr=$2
+
+# Create directory if it doesn't exist
+mkdir -p "$(dirname "$writefile")"
+
+# Write to file and check for errors
+echo "$writestr" > "$writefile"
+if [ $? -ne 0 ]; then
+  echo "Error: could not write to file $writefile"
+  exit 1
+fi
+
+echo "File $writefile written successfully with content: $writestr"
